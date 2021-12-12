832a63d6c4f1978f05bf874a1f432420c2d29bc7
LLRuizO <90738841+LLRuizO@users.noreply.github.com>
2021-12-11 21:42:11 -0500

    change api

--- src/Api/getData.js
+++ src/Api/getData.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getUserList = (page,countPerPage) => {
     return new Promise((resolve,reject)=>{
-        axios.get(`http://localhost:3977/api/V1/dispositivos`).then(res => {
+        axios.get(`https://enigmatic-hollows-40300.herokuapp.com/api/V1/dispositivos`).then(res => {
             resolve(res.data)
         }).catch(err => {
             reject(err)
